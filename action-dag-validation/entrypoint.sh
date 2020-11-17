a47b5cca7ea63ac355da16f243c10845252fe971
Jaya Haryono Manik <jayaharyonomanik@gmail.com>
2020-11-17 23:25:43 +0700

    update entrypoint

--- action-dag-validation/entrypoint.sh
+++ action-dag-validation/entrypoint.sh
@@ -5,8 +5,8 @@ pwd
 
 ls
 
-ls dags
+cd dags
 
-python action-dag-validation/dag_validation_test.py
+python ../action-dag-validation/dag_validation_test.py
 
 echo Hello world my name is $INPUT_MY_NAME

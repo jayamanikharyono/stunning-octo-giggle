f77d2866a66095f7e1e3efc06cb62442f382adc8
Jaya Haryono Manik <jayaharyonomanik@gmail.com>
2020-11-17 15:25:23 +0700

    Update entrypoint.sh

--- action-dag-validation/entrypoint.sh
+++ action-dag-validation/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -l
 sh -c "echo current dir $PWD"
 
-pytest $GITHUB_WORKSPACE/test/dag_validation_test.py
+sh -c "ls"
+
+pytest ./test/dag_validation_test.py
 
 sh -c "echo Hello world my name is $INPUT_MY_NAME"

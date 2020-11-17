34ddf1c360d218a06361ebb9cc8badabb0adf22d
Jaya Haryono Manik <jayaharyonomanik@gmail.com>
2020-11-18 01:08:02 +0700

    Update entrypoint.sh

--- action-dag-validation/entrypoint.sh
+++ action-dag-validation/entrypoint.sh
@@ -3,7 +3,9 @@ echo "Start Testing"
 
 cd dags
 
-pytest ../action-dag-validation/dag_validation_test.py
-#pytest dag_validation_test.py
+#pytest ../action-dag-validation/dag_validation_test.py
+
+cp ../action-dag-validation/dag_validation_test.py dag_validation_test.py
+pytest dag_validation_test.py
 
 echo "Testing Done"

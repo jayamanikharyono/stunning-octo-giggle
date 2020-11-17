1f7778d69cea4f9ac77a11639d4e27bc92057d12
Jaya Haryono Manik <jayaharyonomanik@gmail.com>
2020-11-17 21:55:54 +0700

    Update entrypoint.sh

--- action-dag-validation/entrypoint.sh
+++ action-dag-validation/entrypoint.sh
@@ -8,6 +8,5 @@ sh -c "ls dags"
 #pytest ./test/dag_validation_test.py
 sh -c "python action-dag-validation/dag_validation_test.py"
 
-python action-dag-validation/dag_validation_test.py
 
 sh -c "echo Hello world my name is $INPUT_MY_NAME"

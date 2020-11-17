dc7f293e5cc68ae4a1fcd981501779264b81a07b
Jaya Haryono Manik <jayaharyonomanik@gmail.com>
2020-11-17 15:33:21 +0700

    update dag-validation test

--- action-dag-validation/entrypoint.sh
+++ action-dag-validation/entrypoint.sh
@@ -3,6 +3,7 @@ sh -c "echo current dir $PWD"
 
 sh -c "ls"
 
-pytest ./test/dag_validation_test.py
+#pytest ./test/dag_validation_test.py
+python ./action-dag-validation/dag_validation_test.py
 
 sh -c "echo Hello world my name is $INPUT_MY_NAME"

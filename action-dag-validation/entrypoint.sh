5dce2e73a85e978755fb6a4da2298f09d715ce8b
Jaya Haryono Manik <jayaharyonomanik@gmail.com>
2020-11-18 09:19:58 +0700

    clear up project

--- action-dag-validation/entrypoint.sh
+++ action-dag-validation/entrypoint.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
 echo "Start Testing"
 
-#cd dags
-
 pytest action-dag-validation/dag_validation_test.py
-
-#cp ../action-dag-validation/dag_validation_test.py dag_validation_test.py
-#pytest dag_validation_test.py
-
-echo "Testing Done"
